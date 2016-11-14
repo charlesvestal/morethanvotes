2d274deb1e2234a2baec6e794b9014326058094d
Charles Vestal <charlesv@gmail.com>
2016-11-14 14:56:48 -0800

    a very stupid deploy script

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+echo "paste in your now.sh url: "
+read input_variable
+
+now alias $input_variable morethanvotes.com
+now alias $input_variable www.morethanvotes.com
+now alias $input_variable morethanvotes.org
+now alias $input_variable www.morethanvotes.org
